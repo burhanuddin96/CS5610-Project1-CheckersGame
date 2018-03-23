13834b8dd28c19383be449d1ce4bb4ce07a7b1a6
tongcs <tong.p@husky.neu.edu>
2018-03-23 04:29:51 -0400

    finish basic logic

--- checkers/lib/checkers/game.ex
+++ checkers/lib/checkers/game.ex
@@ -12,6 +12,7 @@ defmodule Checkers.Game do
       		current_player: "dark",
       		checker_selected: -1,
       		jump: false,
+		winner: nil
 		}
 	end
 
@@ -116,9 +117,11 @@ defmodule Checkers.Game do
      	def click_checker(game, i) do
 	IO.inspect "click_checker_or_move"
 		game = clear_moves(game)  # clear previous moves
+		       |> Map.put(:jump, false) # jump change to false
 		       |> add_jumps(i)
 		game = if length(game.moves) == 0 do # can make regular moves only 
 			 add_regular_moves(game, i)  # when there is no jump
+			#else add_regular_moves(game, i)
 		       else game
 		       end
 		Map.put(game, :checker_selected, i) # set selected checker
@@ -126,6 +129,7 @@ defmodule Checkers.Game do
 
 	# clear moves in the given game
 	def clear_moves(game) do
+	IO.inspect "clear_moves"
 		Map.put(game, :moves, [])
 	end
 
@@ -136,6 +140,7 @@ defmodule Checkers.Game do
 			in_light_k(game, i) -> add_jump_light_k(game, i)
 			in_dark_s(game, i) -> add_jump_dark_s(game, i)
 			in_dark_k(game, i) -> add_jump_dark_k(game, i)
+			true -> game
 		end
 	end
 	
@@ -164,6 +169,7 @@ defmodule Checkers.Game do
 		   and in_boundary(x-2, y-2)  
 		   and !in_checkers(game, (x-2)+8*(y-2)) do
 			add_into_moves(game, (x-2)+8*(y-2))
+			|> Map.put(:jump, true)
 		else game
 		end
 	end
@@ -177,6 +183,7 @@ defmodule Checkers.Game do
 		   and in_boundary(x+2, y-2)  
 		   and !in_checkers(game, (x+2)+8*(y-2)) do
 			add_into_moves(game, (x+2)+8*(y-2))
+			|> Map.put(:jump, true)
 		else game
 		end
 	end
@@ -190,6 +197,7 @@ defmodule Checkers.Game do
 		   and in_boundary(x-2, y+2)  
 		   and !in_checkers(game, (x-2)+8*(y+2)) do
 			add_into_moves(game, (x-2)+8*(y+2))
+			|> Map.put(:jump, true)
 		else game
 		end
 	end
@@ -203,6 +211,7 @@ defmodule Checkers.Game do
 		   and in_boundary(x+2, y+2)  
 		   and !in_checkers(game, (x+2)+8*(y+2)) do
 			add_into_moves(game, (x+2)+8*(y+2))
+			|> Map.put(:jump, true)
 		else game
 		end
 	end
@@ -246,6 +255,7 @@ defmodule Checkers.Game do
 		   and in_boundary(x-2, y-2)  
 		   and !in_checkers(game, (x-2)+8*(y-2)) do
 			add_into_moves(game, (x-2)+8*(y-2))
+			|> Map.put(:jump, true)
 		else game
 		end
 	end
@@ -259,6 +269,7 @@ defmodule Checkers.Game do
 		   and in_boundary(x+2, y-2)  
 		   and !in_checkers(game, (x+2)+8*(y-2)) do
 			add_into_moves(game, (x+2)+8*(y-2))
+			|> Map.put(:jump, true)
 		else game
 		end
 	end
@@ -272,6 +283,7 @@ defmodule Checkers.Game do
 		   and in_boundary(x-2, y+2)  
 		   and !in_checkers(game, (x-2)+8*(y+2)) do
 			add_into_moves(game, (x-2)+8*(y+2))
+			|> Map.put(:jump, true)
 		else game
 		end
 	end
@@ -285,6 +297,7 @@ defmodule Checkers.Game do
 		   and in_boundary(x+2, y+2)  
 		   and !in_checkers(game, (x+2)+8*(y+2)) do
 			add_into_moves(game, (x+2)+8*(y+2))
+			|> Map.put(:jump, true)
 		else game
 		end
 	end
@@ -298,7 +311,8 @@ defmodule Checkers.Game do
 			in_light_s(game, i) -> add_regular_moves_light_s(game, i)
 			in_dark_s(game, i) -> add_regular_moves_dark_s(game, i)
 			in_light_k(game, i) -> add_regular_moves_king(game, i)
-			in_dark_k(game, i) -> add_regular_moves_king(game, i)		
+			in_dark_k(game, i) -> add_regular_moves_king(game, i)	
+			true -> game	
 		end
 	end
 
@@ -374,7 +388,275 @@ defmodule Checkers.Game do
 	# handle click on a move
 	def click_move(game, i) do
 	IO.inspect "click move"
-	game
+		if game.jump do
+			click_jump(game, i)
+		else
+			click_regular_move(game, i)
+			|> switch_player()
+			|> clear_moves()
+			|> check_winner()
+		end
+	end
+
+	# switch current player
+	def switch_player(game) do
+	IO.inspect "switch_player"
+		if game.current_player == "dark" do
+			Map.put(game, :current_player, "light")
+		else
+			Map.put(game, :current_player, "dark")
+		end
+	end
+
+	# click a regular move
+	def click_regular_move(game, i) do
+        IO.inspect "click regular move"
+		cond do
+			in_light_s(game, game.checker_selected) -> move_light_s(game, i)
+			in_light_k(game, game.checker_selected) -> move_light_k(game, i)
+			in_dark_s(game, game.checker_selected) -> move_dark_s(game, i)
+			in_dark_k(game, game.checker_selected) -> move_dark_k(game, i)
+			true -> game
+		end
+	end
+
+
+	#####################################################################
+	#regular move on light checker
+	# move a light_s
+	def move_light_s(game, i) do
+		if become_light_king(i) do
+			remove_from_light_s(game, game.checker_selected)
+			|> add_into_light_k(i)
+		else
+			remove_from_light_s(game, game.checker_selected)
+			|> add_into_light_s(i)
+		end
+	end	
+
+	# move a light_k
+	def move_light_k(game, i) do
+		remove_from_light_k(game, game.checker_selected)
+		|> add_into_light_k(i)
+	end
+
+	# checker whether will become a light king
+	def become_light_king(i) do
+		div(i, 8) == 0
+	end
+
+	# remove chekcer from light_s
+	def remove_from_light_s(game, i) do
+		light_s = Enum.filter(game.light_s, fn(x) -> x != i end)
+		Map.put(game, :light_s, light_s)
+	end
+
+	# add chekcer checker into light_s
+	def add_into_light_s(game, i) do
+		light_s = game.light_s ++ [i]
+		Map.put(game, :light_s, light_s)
+	end
+
+	# remove chekcer from light_k
+	def remove_from_light_k(game, i) do
+		light_k = Enum.filter(game.light_k, fn(x) -> x != i end)
+		Map.put(game, :light_k, light_k)
+	end
+
+	# add chekcer checker into light_k
+	def add_into_light_k(game, i) do
+		light_k = game.light_k ++ [i]
+		Map.put(game, :light_k, light_k)
+	end
+        
+	#####################################################################
+	#regular move on dark checker
+        # move a dark_s
+	def move_dark_s(game, i) do
+		if become_dark_king(i) do
+			remove_from_dark_s(game, game.checker_selected)
+			|> add_into_dark_k(i)
+		else
+			remove_from_dark_s(game, game.checker_selected)
+			|> add_into_dark_s(i)
+		end
+	end	
+
+	# move a dark_k
+	def move_dark_k(game, i) do
+		remove_from_dark_k(game, game.checker_selected)
+		|> add_into_dark_k(i)
+	end
+
+	# checker whether will become a dark king
+	def become_dark_king(i) do
+		div(i, 8) == 7
+	end
+
+	# remove chekcer from dark_s
+	def remove_from_dark_s(game, i) do
+		dark_s = Enum.filter(game.dark_s, fn(x) -> x != i end)
+		Map.put(game, :dark_s, dark_s)
+	end
+
+	# add chekcer checker into dark_s
+	def add_into_dark_s(game, i) do
+		dark_s = game.dark_s ++ [i]
+		Map.put(game, :dark_s, dark_s)
+	end
+
+	# remove chekcer from dark_k
+	def remove_from_dark_k(game, i) do
+		dark_k = Enum.filter(game.dark_k, fn(x) -> x != i end)
+		Map.put(game, :dark_k, dark_k)
+	end
+
+	# add chekcer checker into dark_k
+	def add_into_dark_k(game, i) do
+		dark_k = game.dark_k ++ [i]
+		Map.put(game, :dark_k, dark_k)
+	end
+
+
+	##############################################################################
+	# click a jump
+	def click_jump(game, i) do
+        IO.inspect "click jump"
+		cond do
+			in_light_s(game, game.checker_selected) -> jump_light_s(game, i)
+			in_light_k(game, game.checker_selected) -> jump_light_k(game, i)
+			in_dark_s(game, game.checker_selected) -> jump_dark_s(game, i)
+			in_dark_k(game, game.checker_selected) -> jump_dark_k(game, i)
+			true -> game
+		end
+	end
+
+	#####################################################################
+	# jump on light checker
+	# jump a light_s
+	def jump_light_s(game, i) do
+	 y1 = div(i, 8)
+   	 x1 = rem(i, 8)
+	 y2 = div(game.checker_selected, 8)
+   	 x2 = rem(game.checker_selected, 8)
+	 y3 = div(y1 + y2, 2)
+   	 x3 = div(x1 + x2, 2)
+	 game = if become_light_king(i) do
+			remove_from_light_s(game, game.checker_selected)
+			|> add_into_light_k(i)
+			|> remove_from_dark(y3 * 8 + x3)
+		else
+			remove_from_light_s(game, game.checker_selected)
+			|> add_into_light_s(i)
+			|> remove_from_dark(y3 * 8 + x3)
+		end
+	 game = clear_moves(game) # check continues jump
+		|> add_jumps(i)
+	 if length(game.moves) > 0 do
+		click_checker_or_move(game, i)
+	 else
+         	switch_player(game) 
+		|> check_winner()
+	 end
+	end	
+
+	def jump_light_k(game, i) do
+	 y1 = div(i, 8)
+   	 x1 = rem(i, 8)
+	 y2 = div(game.checker_selected, 8)
+   	 x2 = rem(game.checker_selected, 8)
+	 y3 = div(y1 + y2, 2)
+   	 x3 = div(x1 + x2, 2)
+	 game = remove_from_light_k(game, game.checker_selected)
+		|> add_into_light_k(i)
+		|> remove_from_dark(y3 * 8 + x3)
+	 game = clear_moves(game) # check continues jump
+		|> add_jumps(i)
+	 if length(game.moves) > 0 do
+		click_checker_or_move(game, i)
+	 else
+         	switch_player(game) 
+		|> check_winner()
+	 end
+	end	
+	
+	# remove checker from dark
+	def remove_from_dark(game, i) do
+		if in_dark_s(game, i) do
+			remove_from_dark_s(game, i)
+		else
+			remove_from_dark_k(game, i)
+		end
+	end
+
+	#####################################################################
+	# jump on dark checker
+	# jump a dark_s
+	def jump_dark_s(game, i) do
+	 y1 = div(i, 8)
+   	 x1 = rem(i, 8)
+	 y2 = div(game.checker_selected, 8)
+   	 x2 = rem(game.checker_selected, 8)
+	 y3 = div(y1 + y2, 2)
+   	 x3 = div(x1 + x2, 2)
+	 game = if become_dark_king(i) do
+			remove_from_dark_s(game, game.checker_selected)
+			|> add_into_dark_k(i)
+			|> remove_from_light(y3 * 8 + x3)
+		else
+			remove_from_dark_s(game, game.checker_selected)
+			|> add_into_dark_s(i)
+			|> remove_from_light(y3 * 8 + x3)
+		end
+	 game = clear_moves(game) # check continues jump
+		|> add_jumps(i)
+	 if length(game.moves) > 0 do
+		click_checker_or_move(game, i)
+	 else
+         	switch_player(game)
+		|> check_winner()
+	 end
+	end	
+
+	def jump_dark_k(game, i) do
+	 y1 = div(i, 8)
+   	 x1 = rem(i, 8)
+	 y2 = div(game.checker_selected, 8)
+   	 x2 = rem(game.checker_selected, 8)
+	 y3 = div(y1 + y2, 2)
+   	 x3 = div(x1 + x2, 2)
+	 game = remove_from_dark_k(game, game.checker_selected)
+		|> add_into_dark_k(i)
+		|> remove_from_light(y3 * 8 + x3)
+	 game = clear_moves(game) # check continues jump
+		|> add_jumps(i)
+	 if length(game.moves) > 0 do
+		click_checker_or_move(game, i)
+	 else
+         	switch_player(game)
+		|> check_winner()
+	 end
+	end	
+	
+	# remove checker from light
+	def remove_from_light(game, i) do
+		if in_light_s(game, i) do
+			remove_from_light_s(game, i)
+		else
+			remove_from_light_k(game, i)
+		end
+	end
+
+	########################################################################
+	# check winner
+	def check_winner(game) do
+		cond do
+			length(game.light_k) == 0 and length(game.light_s) == 0
+			-> Map.put(game, :winner, "dark")
+			length(game.dark_k) == 0 and length(game.dark_s) == 0
+			-> Map.put(game, :winner, "light")
+			true -> game
+		end
 	end
 
 end
